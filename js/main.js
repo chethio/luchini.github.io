703617181c5b5389470e6742acd4db3f492e794d
Buddhi Thilakshana <hellobachchi@gmail.com>
2022-04-04 12:33:42 +0530

    'aos'

--- js/main.js
+++ js/main.js
@@ -14,8 +14,8 @@
         Preloader
     --------------------*/
   $(window).on("load", function () {
-    $(".loader").delay(1000).fadeOut();
-    $("#preloder").delay(200).fadeOut("slow");
+    // $(".loader").delay(1000).fadeOut();
+    // $("#preloder").delay(200).fadeOut("slow");
 
     /*------------------
             Portfolio filter
